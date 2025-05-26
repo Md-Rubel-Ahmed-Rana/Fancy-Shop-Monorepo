bfff26906b27c9077a44705c17a47bdb49edeaff
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-26 22:45:32 +0600

    Microservice configured for all the services

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,21 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const port = process.env.PORT || 9010;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: Number(port),
+      },
+    }
   );
+
+  await app.listen();
+  Logger.log(`🚀 Auth Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
