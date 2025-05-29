c8ccdc55b10dac263053bf1e144f620ff15f35a3
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-29 18:07:57 +0600

    User service APIs tested from postman and some mismatch fixed

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const port = process.env.PORT || 9001;
@@ -25,6 +26,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+  app.use(morgan('dev'));
 
   app.listen(port, () => {
     Logger.log(`🚀 User Application is running on: http://localhost:${port}`);
