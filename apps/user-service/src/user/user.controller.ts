9f72ed103552d8bd2dbd56b87a6a658ca512dd89
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-05-29 12:46:22 +0600

    User CRUD operation completed

---
+++ apps/user-service/src/user/user.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, UpdateUserDto } from '@fancy-shop/shared-dtos';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    return this.userService.createUser(body);
+  }
+
+  @Get('id/:id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(id);
+  }
+
+  @Get('email/:email')
+  getUserByEmail(@Param('email') email: string) {
+    return this.userService.getUserByEmail(email);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.updateUser(id, body);
+  }
+
+  @Patch(':id/password')
+  updatePassword(@Param('id') id: string, @Body() body: { password: string }) {
+    return this.userService.updatePassword(id, body.password);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
+}
